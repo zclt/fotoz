a4c96cc0dc59bd45aad50d8415ae1d29b048c21e
Matheus Zucolotto <matheus.zclt@gmail.com>
2026-05-03 12:28:16 -0300

    build: configure project with Vite and generate initial static assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
